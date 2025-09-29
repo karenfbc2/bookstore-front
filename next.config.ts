9d444d523d627659f719410f8d2cbf70c77a42aa
karenfbc <k.fuentesb@uniandes.edu.co>
2025-09-28 23:40:47 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   async rewrites() {
     return [
-      { source: "/api/:path*", destination: "http://127.0.0.1:8080/api/:path*" }
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8080/api/:path*",
+      },
     ];
   },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true, 
+  },
 };
+
 export default nextConfig;
